5cf517f74d7a9c868c8a8dbb196808a97b224ca4
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-04 23:16:26 -0500

    Add second test

--- script.test.js
+++ script.test.js
@@ -3,3 +3,7 @@ import { capitalize } from './script';
 test('Capitalize a lowercase word', () => {
     expect(capitalize('test')).toBe('Test');
 });
+
+test('Capitalize an uppercase word', () => {
+    expect(capitalize('TEST')).toBe('Test');
+});
