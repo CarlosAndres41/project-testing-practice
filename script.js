a35d0314971ac855b24435e7bc588543bc84ad1a
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-04 23:27:09 -0500

    Add reverseString function that passes first test

--- script.js
+++ script.js
@@ -11,6 +11,12 @@ function capitalize(string) {
     return result;
 }
 
-function reverseString(string) {}
+function reverseString(string) {
+    let result = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        result += string[i];
+    }
+    return result;
+}
 
 export { capitalize, reverseString };
